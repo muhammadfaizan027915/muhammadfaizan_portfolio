3608d8b7cc5b99f2ee6921043e565d342664ddac
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2024-10-26 23:37:06 +0500

    [Bugfix] Correcting the footer.

--- sections/Footer/index.tsx
+++ sections/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="pt-16 pb-10 md:pt-36 flex justify-center">
-      <div className="flex justify-between items-center gap-4 w-full max-w-7xl text-sm text-blue-100">
+      <div className="flex flex-col md:flex-row justify-between items-center md:gap-4 w-full max-w-7xl text-sm text-blue-100 px-4">
         <p>Copyright &copy; 2024 All rights reserved</p>
         <p>Developed by Muhammad Faizan</p>
       </div>
